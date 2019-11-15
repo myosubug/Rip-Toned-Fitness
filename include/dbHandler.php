e8f35b7ae716facf985130296c191a3cf7f89fe4
myosubug <seanoh.dev@gmail.com>
2019-11-14 19:06:48 -0700

    setting up connection

---
+++ include/dbHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServerName = "localhost";
+$dbUserName = "root"; //if I am using XAMPP, the default username is "root"
+$dbPassWord = ""; //if I am using XAMPP, the default password is ""
+$dbName = "cpsc471"; //db name for my version of phpmyadmin
+
+$connection = mysqli_connect($dbServerNamem, $dbUserName, $dbPassWord, $dbName); //connection requires these 4 variables in order
+
+
---
+++ index.php
@@ -0,0 +1,18 @@
+<!-- connecting db with html file-->
+<?php
+    include_once 'include/dbHandler.php';
+?>
+
+<!-- html from here-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!-- to connect to db from html file after include dbHandler.php, we just need to use "$conn;" -->
+</body>
+</html>
