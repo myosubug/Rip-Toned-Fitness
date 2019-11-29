eb8486a4aa59d127ff7eee56ef5763768c03ac90
unknown <bradent@outlook.com>
2019-11-28 20:29:56 -0700

    started working on customer view and can load images from db, check updated db on discord

--- customer_home.php
+++ customer_home.php
@@ -1,3 +1,7 @@
+<?php
+    include_once 'include/dbHandler.php';
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,8 +13,27 @@
 	session_start();
 	$myUser = $_SESSION['loggedInUser'];
 	echo $myUser['email'];
+	
+	
 ?>
 
+<h3>Product list</h3>
+
+
+
+<?php
+    $sql = "SELECT * FROM product; ";
+    $result = mysqli_query($conn, $sql);
+?>
+
+<?php while($product = mysqli_fetch_assoc($result)){ ?>
+	<div>
+		<img src = "<?php echo $product['image']; ?>">
+		<h3><?php echo $product['name'] ?></h3>
+		<h3><?php echo $product['price'] ?></h3>
+		<a href = "addtocart.php?id=<?php echo $product[id]; ?> role="button">Add to Cart</a>
+	</div>
+<?php } ?>
 
 </body>
 </html>
