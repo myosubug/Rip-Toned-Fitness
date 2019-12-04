8afaf30bfe2ae53f149c615e4909fdc8739d566f
unknown <bradent@outlook.com>
2019-12-03 19:39:27 -0700

    added css to registration

--- customer_registration.php
+++ customer_registration.php
@@ -1,20 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rip Toned Fitness LTD | Login</title>
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
 
+<header>
+    <div class = "container">
+		<nav>
+			<a href = "product.php">Products</a>
+			<a href = "about.php">About</a>
+			<a href = "contact.php">Contact</a>
+			<div id = "branding">
+                <a href = "index.php"><h1><span class = "highlight">Rip Toned </span>Fitness LTD</h1></a>
+			</div>
+			<a href = "login.php">Customer</a>
+			<a href = "employee_login.php">Employee</a>
+			<a href = "admin_login.php">Admin</a>
+        </nav>
+    </div>
+</header>
+
+
+
 <form action="include/customer_registration.inc.php" method="POST">
-	<input type="text" name="username" required placeholder="username">
+	<div><input type="text" name="username" required placeholder="username">
 	<input type="text" name="password" required placeholder="password">
-	<input type="text" name="email" required placeholder="email">
+	<input type="text" name="email" required placeholder="email"></div>
+	<div><h3></h3></div>
+	<div><input type="text" name="city" required placeholder="city">
 	<input type="text" name="province" required placeholder="province">
-	<input type="text" name="country" required placeholder="country">
-	<input type="text" name="city" required placeholder="city">
-	<input type="text" name="unitnumber" required placeholder="unit number">
-	<input type="text" name="streetname" required placeholder="street name">
-	<button type="submit" name="submit">Register</button>
+	<input type="text" name="country" required placeholder="country"></div>
+	<div><h3></h3></div>
+	<div><input type="text" name="unitnumber" required placeholder="unit number">
+	<input type="text" name="streetname" required placeholder="street name"></div>
+	<div><h3></h3></div>
+	<div><button type="submit" name="submit">Register</button></div>
 </form>
 
 </body>
--- registration_success.php
+++ registration_success.php
@@ -1,13 +1,34 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rip Toned Fitness LTD | Login</title>
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
 
-<p>
+<header>
+    <div class = "container">
+		<nav>
+			<a href = "product.php">Products</a>
+			<a href = "about.php">About</a>
+			<a href = "contact.php">Contact</a>
+			<div id = "branding">
+                <a href = "index.php"><h1><span class = "highlight">Rip Toned </span>Fitness LTD</h1></a>
+			</div>
+			<a href = "login.php">Customer</a>
+			<a href = "employee_login.php">Employee</a>
+			<a href = "admin_login.php">Admin</a>
+        </nav>
+    </div>
+</header>
+
+<h3>
 	Registration Success!
-</p>
+</h3>
 
 <a href="login.php"> Return to login </a>
 
